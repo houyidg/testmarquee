8a6a575d666a558605f20c621f569b92a1488da9
houyidg <511700417@qq.com>
2023-09-22 01:30:08 +0800

    x

--- app/src/main/java/com/wl/handdanmu/views/MarqueeTextView2.kt
+++ app/src/main/java/com/wl/handdanmu/views/MarqueeTextView2.kt
@@ -4,20 +4,27 @@ import android.content.Context
 import android.graphics.Canvas
 import android.graphics.Color
 import android.graphics.Paint
+import android.graphics.Rect
+import android.text.TextPaint
 import android.util.AttributeSet
 import android.view.View
 
+
 class MarqueeTextView2(context: Context, attrs: AttributeSet?) : View(context, attrs), ITextSetUp {
-    private var text ="This is danMu"
-         // 要显示的文本
+    private var text = "This is danMu"
+
+    // 要显示的文本
     private var textColor = Color.WHITE // 文本颜色
     private var textSize = 60f // 文本大小
     private val textPaint = Paint()
     private var textX = 0f // 文本的X坐标位置
-    private val defaultSpeed = 20f
+    private val defaultSpeed = 10f
     private var scrollSpeed = defaultSpeed // 滚动速度，可以根据需要调整
 
     private var viewWidth = 0
+    val duration = 20L
+
+    private var textY = 0f
 
     init {
         textPaint.color = textColor
@@ -33,38 +40,46 @@ class MarqueeTextView2(context: Context, attrs: AttributeSet?) : View(context, a
 
     override fun onDraw(canvas: Canvas) {
         super.onDraw(canvas)
-        canvas.drawText(text, textX, (height / 2).toFloat(), textPaint)
+        textY = (height / 2).toFloat() + getTxtHeight() / 2
+        canvas.drawText(text, textX, textY, textPaint)
     }
 
+    var textWidth = 0f
     fun startMarquee() {
         val textWidth = textPaint.measureText(text)
-        val duration = 50
         val marqueeRunnable = object : Runnable {
             override fun run() {
                 textX -= scrollSpeed
-                if (textX + textWidth < 0) {
+                if (textX < -textWidth * 4) {
                     textX = viewWidth.toFloat()
                 }
                 invalidate()
-                postDelayed(this, 16)
+                postDelayed(this, duration)
             }
         }
         removeCallbacks(marqueeRunnable)
-        postDelayed(marqueeRunnable, duration.toLong())
+        postDelayed(marqueeRunnable, duration)
     }
 
     override fun setTextSize(size: Float) {
-        textSize = size*4
+        textSize = size * 4
         textPaint.textSize = textSize
+        textWidth = textPaint.measureText(text)
+        textY = (height / 2).toFloat() + getTxtHeight() / 2
+    }
+
+    private fun getTxtHeight(): Int {
+        return textSize.toInt()// textBounds.height()
     }
 
     override fun setText(txt: String) {
         text = txt
+        textWidth = textPaint.measureText(text)
     }
 
     override fun setScrollSpeed(speed: Float) {
-        scrollSpeed = speed*defaultSpeed
-        scrollSpeed = Math.max(scrollSpeed,defaultSpeed)
+        scrollSpeed = speed * defaultSpeed
+        scrollSpeed = Math.max(scrollSpeed, defaultSpeed)
     }
 
     override fun setTextColor(color: Int) {
